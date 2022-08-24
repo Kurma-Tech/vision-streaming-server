1e9d0c84764458d3de6318fb59d45615fb8b3ae9
suzanpradhan <74817169+suzanpradhan@users.noreply.github.com>
2022-08-24 13:43:07 +0545

    Update stream.service.ts

--- src/services/stream.service.ts
+++ src/services/stream.service.ts
@@ -25,21 +25,21 @@ class StreamService{
                 // fs.unlinkSync(`${directory}/output.m3u8`);
                 try {
                     fs.unlink(path.join(directory, 'output.m3u8'), err => {
-                        if (err) console.log(err);
+
                     });
                 } catch (error) {
                     
                 }
                 
-                // fs.readdir(directory, (err, files) => {
-                //     if (err) throw err;
+                fs.readdir(directory, (err, files) => {
+                    if (err) return;
 
-                //     for (const file of files) {
-                //         fs.unlink(path.join(directory, file), err => {
-                //             if (err) throw err;
-                //         });
-                //     }
-                // });
+                    for (const file of files) {
+                        fs.unlink(path.join(directory, file), err => {
+                            
+                        });
+                    }
+                });
             }
 
             // ffmpeg service
